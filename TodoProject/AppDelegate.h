56b3c55773c05d2bbe90ba64c471cc46b0db9d21
Mostafa Samir <mostafasamir@Mostafas-Mac.local>
2019-12-14 16:25:19 +0200

    Initial Commit

---
+++ TodoProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TodoProject
+//
+//  Created by Mostafa Samir on 12/14/19.
+//  Copyright © 2019 Mostafa Samir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TodoProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TodoProject
+//
+//  Created by Mostafa Samir on 12/14/19.
+//  Copyright © 2019 Mostafa Samir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
